d4ace18e641c36ca319c4426da52e112563a30ce
Eduardo Savian <eduardosavian@outlook.com>
2024-05-12 18:04:53 -0300

    Add main

--- src/main.go
+++ src/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+  "strconv"
+)
 
 func main() {
-  fmt.Println("Hello World")
+  fmt.Println("Hello, world")
+
+  if length := len(os.Args);  length < 3 || length > 3 {
+    panic("Format invalid, use go run x y")
+  }
+
+  x, err := strconv.Atoi(os.Args[1])
+  if err != nil {
+      panic(err)
+  }
+
+  y, err := strconv.Atoi(os.Args[2])
+  if err != nil {
+      panic(err)
+  }
+
+  var chess [8][8]int;
+  chess[x][y] = 1
+
+  print(chess[x][y])
 }
