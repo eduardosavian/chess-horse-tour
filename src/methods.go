700cbde76b4d3467fe6cd0b8e9372fdeb093eb69
Eduardo Savian <eduardosavian@outlook.com>
2024-05-13 20:13:23 -0300

    Waldorf working

--- src/methods.go
+++ src/methods.go
@@ -13,67 +13,56 @@ type Move struct {
 	Priority int
 }
 
-func findNextMoves(x, y, boardSize int, method string) [][]int {
+func findNextMoves(x, y, boardSize int, method string) []Move {
 	possibleMoves := [][]int{
 		{-2, -1}, {-1, -2}, {1, -2}, {2, -1},
 		{2, 1}, {1, 2}, {-1, 2}, {-2, 1},
 	}
 
-	validMoves := [][]int{}
+	validMoves := []Move{}
 
 	for _, move := range possibleMoves {
 		nextX := x + move[0]
 		nextY := y + move[1]
 
 		if isMoveValid(nextX, nextY, boardSize) {
-			validMoves = append(validMoves, []int{nextX, nextY})
+			validMoves = append(validMoves, Move{nextX, nextY, 0})
 		}
 	}
 
 	if method == "warnsdorff" {
-		moves := make([]Move, 0)
-
-		for _, move := range validMoves {
-			nextX, nextY := move[0], move[1]
-			validMovesFromNext := len(findNextMoves(nextX, nextY, boardSize, "default"))
-			moves = append(moves, Move{nextX, nextY, validMovesFromNext})
+		for i := range validMoves {
+			move := &validMoves[i]
+			move.Priority = len(findNextMoves(move.X, move.Y, boardSize, "default"))
 		}
 
-		sort.Slice(moves, func(i, j int) bool {
-			return moves[i].Priority < moves[j].Priority
+		sort.Slice(validMoves, func(i, j int) bool {
+			return validMoves[i].Priority < validMoves[j].Priority
 		})
-
-		sortedMoves := [][]int{}
-		for _, move := range moves {
-			sortedMoves = append(sortedMoves, []int{move.X, move.Y})
-		}
-
-		return sortedMoves
 	}
 
 	return validMoves
 }
 
+
 func backtrack(board [][]int, moveNum, x, y, boardSize int, method string) bool {
+	board[x][y] = moveNum
+
 	if moveNum == boardSize*boardSize {
-		board[x][y] = moveNum
 		return true
 	}
 
 	nextMoves := findNextMoves(x, y, boardSize, method)
 
 	for _, move := range nextMoves {
-		nextX, nextY := move[0], move[1]
+		nextX, nextY := move.X, move.Y
 		if board[nextX][nextY] == 0 {
-			board[nextX][nextY] = moveNum
-
 			if backtrack(board, moveNum+1, nextX, nextY, boardSize, method) {
 				return true
 			}
-
-			board[nextX][nextY] = 0
 		}
 	}
 
+	board[x][y] = 0 
 	return false
 }
