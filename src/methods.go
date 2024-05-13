6c19346925562635e8a163fe69f7965b0ba30b76
Eduardo Savian <eduardosavian@outlook.com>
2024-05-12 21:03:41 -0300

    Add new algorith

--- src/methods.go
+++ src/methods.go
@@ -129,3 +129,63 @@ func dijkstra(board [][]int, moveNum, x, y, boardSize int) bool {
 
 	return false
 }
+
+func dijkstraTourWorker(startX, startY, boardSize int) {
+	defer waitGroup.Done()
+
+	board := make([][]int, boardSize)
+	for i := range board {
+		board[i] = make([]int, boardSize)
+	}
+
+	boardMutex.Lock()
+	board[startX][startY] = 1
+	boardMutex.Unlock()
+
+	if dijkstraBacktrack(board, 1, startX, startY, boardSize) {
+		select {
+		case foundTour <- board:
+			close(stopSearch)
+		default:
+		}
+	}
+}
+
+func dijkstraBacktrack(board [][]int, moveNum, x, y, boardSize int) bool {
+	if moveNum == boardSize*boardSize {
+		return true
+	}
+
+	select {
+	case <-stopSearch:
+		return false
+	default:
+	}
+
+	// Implement Dijkstra's algorithm or a modified search logic here
+	// Adjust how you explore the board and make decisions for the next move
+
+	nextMoves := findNextMoves(x, y, boardSize)
+	rand.Shuffle(len(nextMoves), func(i, j int) {
+		nextMoves[i], nextMoves[j] = nextMoves[j], nextMoves[i]
+	})
+
+	for _, move := range nextMoves {
+		nextX, nextY := move[0], move[1]
+		if board[nextX][nextY] == 0 {
+			boardMutex.Lock()
+			board[nextX][nextY] = moveNum + 1
+			boardMutex.Unlock()
+
+			if dijkstraBacktrack(board, moveNum+1, nextX, nextY, boardSize) {
+				return true
+			}
+
+			boardMutex.Lock()
+			board[nextX][nextY] = 0
+			boardMutex.Unlock()
+		}
+	}
+
+	return false
+}
